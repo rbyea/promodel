62b3288469121799acb8143f42820b85090ffd8f
optimist23 <opt1m1st23@yandex.ru>
2019-04-23 15:58:59 +0500

    copy scripts to common.js

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	var instance = $('#promo-video').data('vide');
+
+	$("#menu").click(function () {
+		$(".hum").toggleClass("is-active");
+	});
+
+	$('.hamburger').on('click', function () {
+		$('.header-navbar__mobile').toggleClass('header-menu_vis');
+	});
+	$('.header-navbar__mobile_close').on('click', function () {
+		$('.header-navbar__mobile').toggleClass('header-menu_vis');
+	});
+
+		$(".header-navbar__mobile_close").click(function () {
+				$(".hum").toggleClass("is-active");
+			});
+
+		$('#btn-head').on('click', function () {
+			$('.modal').toggleClass('modal-vis');
+		});
+		$('.modal-close').on('click', function () {
+				$('.modal').toggleClass('modal-vis');
+		});
+});
